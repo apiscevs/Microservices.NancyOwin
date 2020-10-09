8c33a5db239de1782c10eda11919125bff2e1f5e
Aleksejs Piscevs <aleksejs.piscevs@gmail.com>
2020-10-09 16:33:58 -0400

    hello world app

---
+++ HelloWorld/HelloMicroservices/HelloMicroservices/NancyModules/CurrentDateTimeModule.cs
@@ -0,0 +1,17 @@
+﻿using Nancy;
+using Nancy.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloMicroservices.NancyModules
+{
+    public class CurrentDateTimeModule : NancyModule
+    {
+        public CurrentDateTimeModule()
+        {
+            Get("/", _ => DateTime.UtcNow);
+        }
+    }
+}
---
+++ HelloWorld/HelloMicroservices/HelloMicroservices/Startup.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Nancy.Owin;
+
+namespace HelloMicroservices
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // If using Kestrel:
+            services.Configure<KestrelServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
+
+            // If using IIS:
+            services.Configure<IISServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseOwin(buildFunc => {
+
+                buildFunc(next => env =>
+                {
+                    Console.WriteLine("Got request");
+                    return next(env);
+                });
+
+                buildFunc.UseNancy();
+            });
+
+
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            //app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
+        }
+    }
+}
