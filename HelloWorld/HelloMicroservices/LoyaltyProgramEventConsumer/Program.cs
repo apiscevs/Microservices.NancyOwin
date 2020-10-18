422229fd4f09706f42c8f48508bc89c389970d9c
Aleksejs Piscevs <aleksejs.piscevs@gmail.com>
2020-10-18 17:59:11 -0400

    simple data feed

---
+++ HelloWorld/HelloMicroservices/LoyaltyProgramEventConsumer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LoyaltyProgramEventConsumer
+{
+    class Program
+    {
+        private static EventSubscriber subscriber;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            EventSubscriber subscriber = new EventSubscriber("127.0.0.1:5050");
+            subscriber.Start();
+            //Run(this);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HelloWorld/HelloMicroservices/SpecialOffers.EventFeed/IEventStore.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace SpecialOffers.EventFeed
+{
+    public interface IEventStore
+    {
+        IEnumerable<Event> GetEvents(long firstEventSequenceNumber, long lastEventSequenceNumber);
+        void Raise(string eventName, object content);
+    }
+}
