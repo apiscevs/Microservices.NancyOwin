bef1b83a643c763e00dc38a11e036a8b63c21f8d
Aleksejs Piscevs <aleksejs.piscevs@gmail.com>
2020-10-25 14:26:20 -0400

    event store

--- HelloWorld/HelloMicroservices/ShoppingCart/EventFeed/EventStore.cs
+++ HelloWorld/HelloMicroservices/ShoppingCart/EventFeed/EventStore.cs
@@ -1,34 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+using Newtonsoft.Json;
 
 namespace ShoppingCart.EventFeed
 {
     public class EventStore : IEventStore
     {
-        private static long currentSequenceNumber = 0;
-        private static readonly IList<Event> database = new List<Event>();
-
-        public IEnumerable<Event> GetEvents(
-          long firstEventSequenceNumber,
-          long lastEventSequenceNumber)
-          => database
-              .Where(e =>
-                e.SequenceNumber >= firstEventSequenceNumber &&
-                e.SequenceNumber <= lastEventSequenceNumber)
-              .OrderBy(e => e.SequenceNumber);
-
-        public void Raise(string eventName, object content)
+        private string connectionString =
+        @"Data Source=STONE037\SQLEXPRESS;Initial Catalog=ShoppingCart;Integrated Security=True";
+        private const string writeEventSql =
+        @"insert into EventStore(Name, OccurredAt, Content) values (@Name, @OccurredAt, @Content)";
+        public async Task Raise(string eventName, object content)
         {
-            var seqNumber = Interlocked.Increment(ref currentSequenceNumber);
-            database.Add(
-              new Event(
-                seqNumber,
-                DateTimeOffset.UtcNow,
-                eventName,
-                content));
+            var jsonContent = JsonConvert.SerializeObject(content);
+            try
+            {
+                using (var conn = new SqlConnection(connectionString))
+                {
+                    await conn.ExecuteAsync(
+                    writeEventSql,
+                    new
+                    {
+                        Name = eventName,
+                        OccurredAt = DateTimeOffset.Now,
+                        Content = jsonContent
+                    });
+                }
+            }
+            catch(Exception e)
+            {
+                throw;
+            }
+        }
+
+        private const string readEventsSql =
+@"select * from EventStore where ID >= @Start and ID <= @End";
+        public async Task<IEnumerable<Event>> GetEvents(
+        long firstEventSequenceNumber,
+        long lastEventSequenceNumber)
+        {
+            try
+            {
+                using (var conn = new SqlConnection(connectionString))
+                {
+                    return (await conn.QueryAsync<dynamic>(
+                    readEventsSql,
+                    new
+                    {
+                        Start = firstEventSequenceNumber,
+                        End = lastEventSequenceNumber
+                    }).ConfigureAwait(false))
+                    .Select(row =>
+                    {
+                        var content = JsonConvert.DeserializeObject(row.Content);
+                        return new Event(row.ID, row.OccurredAt, row.Name, content);
+                    });
+                }
+            } catch(Exception e)
+            {
+                throw;
+            }
+
         }
     }
 }
